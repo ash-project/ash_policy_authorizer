2cf6790a650caff805f556636abcdad5d4862c2a
Zach Daniel <zachary.s.daniel@gmail.com>
2020-06-01 01:34:55 -0400

    chore: add docs libs

--- mix.exs
+++ mix.exs
@@ -3,13 +3,31 @@ defmodule AshPolicyAccess.MixProject do
 
   @version "0.1.0"
 
+  @description """
+  A policy based access authorizer for the Ash Framework
+  """
+
   def project do
     [
       app: :ash_policy_access,
       version: @version,
+      package: package(),
+      description: @description,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/ash-project/ash_policy_access",
+      homepage_url: "https://github.com/ash-project/ash_policy_access"
+    ]
+  end
+
+  def package() do
+    [
+      name: :ash_policy_access,
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/ash-project/ash_policy_access"
+      }
     ]
   end
 
@@ -25,6 +43,7 @@ defmodule AshPolicyAccess.MixProject do
     [
       {:picosat_elixir, "~> 0.1.1"},
       {:git_ops, "~> 2.0.0", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:ash, "~> 0.1.2"}
     ]
   end
